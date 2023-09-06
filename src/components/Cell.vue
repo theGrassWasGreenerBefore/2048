ac489e385654057145eaffcb616876b1047cebd5
MeGoogle <manonhorizon@gmail.com>
2023-09-07 00:29:32 +0300

    cell as separate component

---
+++ src/components/Cell.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  value: {
+    type: String,
+    required: true
+  },
+  x: {
+    type: Number,
+    required: true
+  },
+  y: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="cell" :style="{ 'left': `${x * 100}px`, 'top': `${y * 100}px` }">
+    {{ value }}
+  </div>
+</template>
+
+<style scoped>
+.cell {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  background: green;
+  text-align: center;
+  font-size: 60px;
+  color: white;
+  padding: 20px 0;
+}
+</style>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="cells_container">
-    <div class="cell-wrap" v-for="cell in cells" :key="cell.id">
-      <div class="cell" v-bind:style="{ 'left': `${cell.x * 100}px`, 'top': `${cell.y * 100}px` }">
-        {{ cell.value }}
-      </div>
-    </div>
+    <Cell v-for="cell in cells" :key="cell.id" :value="cell.value" :x="cell.x" :y="cell.y" />
   </div>
 </template>
 
 <script>
+import Cell from '@/components/Cell.vue'
 export default {
+  components: {
+    Cell
+  },
   data() {
     return {
       cells: [{ id: 'e3432e', x: 1, y: 1, value: 35 }, { id: '3e432e', x: 2, y: 2, value: 56 }]
@@ -25,22 +25,5 @@ export default {
   width: 400px;
   height: 400px;
   border: 5px solid #333;
-}
-.cell-wrap {
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 0;
-  height: 0;
-}
-.cell {
-  position: absolute;
-  width: 100px;
-  height: 100px;
-  background: green;
-  text-align: center;
-  font-size: 60px;
-  color: white;
-  padding: 20px 0;
 }
 </style>
